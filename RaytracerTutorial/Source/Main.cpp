957ea75bcef6b29d950a5082c0be90d1cbc93f7a
CometStorm991 <cometstorm991@gmail.com>
2025-07-13 21:18:56 -0400

    Rendered sphere

--- RaytracerTutorial/Source/Main.cpp
+++ RaytracerTutorial/Source/Main.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <cstdint>
 #include <iostream>
 #include <vector>
@@ -9,33 +10,77 @@
 
 void writeColor(std::vector<uint8_t>& imageData, glm::vec3 color)
 {
-	imageData.push_back(static_cast<uint8_t>(color.x * 256.0f));
-	imageData.push_back(static_cast<uint8_t>(color.y * 256.0f));
-	imageData.push_back(static_cast<uint8_t>(color.z * 256.0f));
+	imageData.push_back(static_cast<uint8_t>(color.x * 255.0f));
+	imageData.push_back(static_cast<uint8_t>(color.y * 255.0f));
+	imageData.push_back(static_cast<uint8_t>(color.z * 255.0f));
+}
+
+bool hitSphere(const Ray& ray)
+{
+	glm::vec3 center = glm::vec3(0, 0, -3);
+	float radius = 1.0f;
+
+	const glm::vec3& q = ray.getOrigin();
+	const glm::vec3& d = ray.getDir();
+	
+	float a = glm::dot(d, d);
+	float b = glm::dot(-2.0f * d, center - q);
+	float c = glm::dot(center - q, center - q) - radius * radius;
+
+	float discriminant = b * b - 4.0f * a * c;
+	// 36 - 4 * 1 * 8
+	return discriminant >= 0;
+}
+
+glm::vec3 rayColor(const Ray& ray)
+{
+	if (hitSphere(ray))
+	{
+		return glm::vec3(0.0f, 0.5f, 1.0f);
+	}
+	return glm::vec3(0.0f, 0.0f, 0.0f);
 }
 
 int main()
 {
-	const unsigned int imageWidth = 1920;
-	const unsigned int imageHeight = 1080;
-	const float aspectRatio = imageWidth / imageHeight;
+	const uint32_t imageWidth = 2560;
+	const uint32_t imageHeight = 1600;
+	const float aspectRatio = static_cast<float>(imageWidth) / imageHeight;
 	
 	const float viewportHeight = 2.0f;
 	const float viewportWidth = viewportHeight * aspectRatio;
 
+	const float rayDeltaX = viewportWidth / imageWidth;
+	const float rayDeltaY = -viewportHeight / imageHeight;
+	// glm::vec2 rayDelta = glm::vec2(rayDeltaX, rayDeltaY);
+	const float rayInitX = -viewportWidth / 2.0f + 0.5f * rayDeltaX;
+	const float rayInitY = viewportHeight / 2.0f + 0.5f * rayDeltaY;
 
+	glm::vec3 cameraPos = glm::vec3(0, 0, 0);
 
 	std::vector<uint8_t> imageData;
 
-	for (unsigned int i = 0; i < imageHeight; i++)
+	for (uint32_t i = 0; i < imageHeight; i++)
 	{
-		for (unsigned int j = 0; j < imageWidth; j++)
+		for (uint32_t j = 0; j < imageWidth; j++)
 		{
-			float r = static_cast<float>(i) / (imageHeight - 1);
-			float g = static_cast<float>(j) / (imageWidth - 1);
-			float b = 0.0f;
+			float y = rayInitY + rayDeltaY * i;
+			float x = rayInitX + rayDeltaX * j;
+			float z = -1.0f;
 
-			writeColor(imageData, glm::vec3(r, g, b));
+			glm::vec3 viewportPos = glm::vec3(x, y, z);
+			// std::cout << x << " " << y << " " << z << std::endl;
+			Ray ray = Ray(cameraPos, viewportPos - cameraPos);
+
+			glm::vec3 color = rayColor(ray);
+
+			writeColor(imageData, glm::vec3(color.x, color.y, color.z));
+		}
+
+		uint32_t hundredth = static_cast<uint32_t>(imageHeight / 100.0f);
+		if ((i + 1) % hundredth == 0)
+		{
+			std::cout << "Progress: " << static_cast<float>(i + 1) / imageHeight << std::endl;
 		}
 	}
 
